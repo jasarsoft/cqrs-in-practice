82449400a814e7d5b9d997c98a171894015b2b1f
Edin Jašarević <edinjasar14@gmail.com>
2022-02-23 21:30:56 +0100

    dealing with create and delete method

--- src/Api/Dtos/StudentDto.cs
+++ src/Api/Dtos/StudentDto.cs
@@ -8,13 +8,24 @@
 
         public string Course1 { get; set; }
         public string Course1Grade { get; set; }
-        public string Course1DisenrollmentComment { get; set; }
         public int? Course1Credits { get; set; }
 
         public string Course2 { get; set; }
         public string Course2Grade { get; set; }
-        public string Course2DisenrollmentComment { get; set; }
         public int? Course2Credits { get; set; }
+    }
+
+    public sealed class NewStudentDto
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public string Course1 { get; set; }
+        public string Course1Grade { get; set; }
+        
+        public string Course2 { get; set; }
+        public string Course2Grade { get; set; }
     }
 
     public sealed class StudentEnrollmentDto
